e40919fb1010090326be0e6dff351ed7079a02d0
Sam Richards <sbrichards@gmail.com>
2019-12-05 13:29:32 -0800

    Add gatsby-background-image

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,40 +1,26 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+
+import StyledFullBackground from "./FullBackground"
 
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>© Ethereum Foundation, {new Date().getFullYear()}</footer>
-      </div>
+      <StyledFullBackground>
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0px 1.0875rem 1.45rem`,
+            paddingTop: 0,
+          }}
+        >
+          <main>{children}</main>
+          <footer>© Ethereum Foundation, {new Date().getFullYear()}</footer>
+        </div>
+      </StyledFullBackground>
     </>
   )
 }
