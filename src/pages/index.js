20d51cd60f03051a2427c412c60c6ed385bea612
Sam Richards <sbrichards@gmail.com>
2019-12-10 08:48:17 -0800

    Test update files to lowercase

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import Img from "gatsby-image"
 
-import Layout from "../components/Layout"
-import SEO from "../components/SEO"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const IndexPage = ({ data }) => (
   <Layout>
