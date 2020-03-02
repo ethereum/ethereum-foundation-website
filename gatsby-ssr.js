5c0fc03ba07d8b3f1f87ab12d9900b3cc3bc1a5a
Sam Richards <sbrichards@gmail.com>
2020-03-02 05:10:33 +0000

    Replace vanta with background image

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,13 +10,3 @@ import Layout from "./src/components/Layout"
 export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>
 }
-
-export const onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([
-    <script
-      key="1"
-      type="text/javascript"
-      src="https://cdnjs.cloudflare.com/ajax/libs/three.js/110/three.min.js"
-    />,
-  ])
-}
