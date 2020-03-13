9d3f93c568e5837b195643cc0691d68a73937e66
Sam Richards <sbrichards@gmail.com>
2020-03-13 10:32:12 -0700

    Remove board bios

--- src/pages/about/board.js
+++ src/pages/about/board.js
@@ -55,22 +55,19 @@ const ExecutiveBoardPage = () => {
     {
       name: "Aya Miyaguchi",
       title: "Executive Director",
-      bio:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.",
+      bio: "",
       img: data.aya.childImageSharp.fixed,
     },
     {
       name: "Vitalik Buterin",
       title: "Co-founder of Ethereum",
-      bio:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.",
+      bio: "",
       img: data.vitalik.childImageSharp.fixed,
     },
     {
       name: "Patrick Storcheneger",
-      title: "",
-      bio:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.",
+      title: "Board member",
+      bio: "",
       img: data.patrick.childImageSharp.fixed,
     },
   ]
