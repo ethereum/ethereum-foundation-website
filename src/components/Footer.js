f109e97636b6ae64a74e7acd3fc77ec10f961663
ph5500 <philfives@gmail.com>
2020-06-04 18:17:50 -0600

    quick change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -204,7 +204,7 @@ const Footer = ({ isOpen, toggleFooter }) => {
                 >
                   Privacy Policy
                 </OutboundLink>
-                <Link to="/cookie-policy/">Cookie Policy</Link>
+                <Link to="/cookie-policy/">Cookie Police</Link>
               </FooterDivLinks>
             </FooterContentDiv>
           </StyledFooter>
