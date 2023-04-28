22deb55429d1c37b48108e93a4f1bd6f8ae57a75
lassejaco <lasse.jacobsen@ethereum.org>
2023-04-28 17:44:43 +0900

    Clearer UX around showing page content. Add a little padding to footer mobile foldout.

--- src/utils/useScrollDirection.ts
+++ src/utils/useScrollDirection.ts
@@ -1,48 +1,68 @@
-import React from 'react';
-import { pageContentID } from 'components/page/Content';
+import React from "react"
+import { pageContentID } from "components/page/Content"
 
 export enum ScrollDirection {
-  UP = 'up',
-  DOWN = 'down'
+  UP = "up",
+  DOWN = "down",
 }
 
-let xDown = 0;
-let yDown = 0;
+let xDown = 0
+let yDown = 0
 
 const checkIsContentScrolled = () => {
   const scrollableContentElement = document.getElementById(pageContentID)
-      
-  if (!scrollableContentElement) return false;
 
-  return scrollableContentElement?.scrollTop > 0;
+  if (!scrollableContentElement) return false
+
+  return scrollableContentElement?.scrollTop > 0
 }
 
 function getTouches(event: any) {
-    return event.touches || event.originalEvent.touches;
+  return event.touches || event.originalEvent.touches
 }
 
 // TODO: Should probably rethink this and make it more "reacty" - e.g. use a gesture library and apply the listeners to the components that need them directly rather than using window listeners
 const useScrollDirection = () => {
-  const [scrollDirection, setScrollDirection] = React.useState<ScrollDirection>(ScrollDirection.UP);
+  const [scrollDirection, setScrollDirection] = React.useState<ScrollDirection>(
+    ScrollDirection.UP
+  )
+
+  React.useEffect(() => {
+    const scrollIndicator = document.getElementById("scroll-indicator")
+
+    if (!scrollIndicator) return
+
+    const forceScroll = () => {
+      setScrollDirection(ScrollDirection.DOWN)
+    }
+
+    scrollIndicator.addEventListener("mousedown", forceScroll)
+    scrollIndicator.addEventListener("touchstart", forceScroll)
+
+    return () => {
+      scrollIndicator.removeEventListener("mousedown", forceScroll)
+      scrollIndicator.removeEventListener("touchstart", forceScroll)
+    }
+  }, [])
 
   React.useEffect(() => {
     const handleScroll = (e: any) => {
-      const scrolledDown = e.deltaY > 0;
-      const direction = scrolledDown ? ScrollDirection.DOWN : ScrollDirection.UP;
+      const scrolledDown = e.deltaY > 0
+      const direction = scrolledDown ? ScrollDirection.DOWN : ScrollDirection.UP
 
-      const isContentScrolled = checkIsContentScrolled();
-      
+      const isContentScrolled = checkIsContentScrolled()
+
       if (isContentScrolled) {
-        setScrollDirection(ScrollDirection.DOWN);
+        setScrollDirection(ScrollDirection.DOWN)
       } else {
-        setScrollDirection(direction);
+        setScrollDirection(direction)
       }
     }
 
     const handleTouchStart = (e: any) => {
-      const firstTouch = getTouches(e)[0];
-      xDown = firstTouch.clientX;
-      yDown = firstTouch.clientY;
+      const firstTouch = getTouches(e)[0]
+      xDown = firstTouch.clientX
+      yDown = firstTouch.clientY
     }
 
     const handleTouchMove = (e: any) => {
@@ -50,44 +70,43 @@ const useScrollDirection = () => {
       //     return;
       // }
 
-      var xUp = e.touches[0].clientX;
-      var yUp = e.touches[0].clientY;
+      var xUp = e.touches[0].clientX
+      var yUp = e.touches[0].clientY
 
-      var xDiff = xDown - xUp;
-      var yDiff = yDown - yUp;
+      var xDiff = xDown - xUp
+      var yDiff = yDown - yUp
 
       if (Math.abs(xDiff) > Math.abs(yDiff)) {
-          if (xDiff > 0) {
-              // Right swipe
-          } else {
-              // Left swipe
-          }
+        if (xDiff > 0) {
+          // Right swipe
+        } else {
+          // Left swipe
+        }
       } else {
-        const isContentScrolled = checkIsContentScrolled();
+        const isContentScrolled = checkIsContentScrolled()
 
         if (isContentScrolled || yDiff > 0) {
-          setScrollDirection(ScrollDirection.DOWN);
+          setScrollDirection(ScrollDirection.DOWN)
         } else {
-          setScrollDirection(ScrollDirection.UP);
+          setScrollDirection(ScrollDirection.UP)
         }
       }
     }
 
-    window.addEventListener('wheel', handleScroll)
-    window.addEventListener('scroll', handleScroll)
-    window.addEventListener('touchstart', handleTouchStart)
-    window.addEventListener('touchmove', handleTouchMove)
+    window.addEventListener("wheel", handleScroll)
+    window.addEventListener("scroll", handleScroll)
+    window.addEventListener("touchstart", handleTouchStart)
+    window.addEventListener("touchmove", handleTouchMove)
 
     return () => {
-      window.removeEventListener('wheel', handleScroll)
-      window.removeEventListener('scroll', handleScroll)
-      window.removeEventListener('touchstart', handleTouchStart)
-      window.removeEventListener('touchmove', handleTouchMove)
+      window.removeEventListener("wheel", handleScroll)
+      window.removeEventListener("scroll", handleScroll)
+      window.removeEventListener("touchstart", handleTouchStart)
+      window.removeEventListener("touchmove", handleTouchMove)
     }
   }, [])
 
-  return scrollDirection;
+  return scrollDirection
 }
 
-
-export default useScrollDirection;
+export default useScrollDirection
