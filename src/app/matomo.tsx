801512f1132d262e046213d51075eecf9b631d51
Pablo Pettinari <pettinarip@gmail.com>
2026-03-03 18:24:47 +0100

    Disable cookies and remove package-lock.json
    - Set disableCookies: true for privacy-first tracking
    - Remove package-lock.json since project uses yarn

--- src/app/matomo.tsx
+++ src/app/matomo.tsx
@@ -19,6 +19,7 @@ export default function MatomoAnalytics() {
       siteId: MATOMO_SITE_ID,
       pathname,
       searchParams,
+      disableCookies: true,
     })
   }, [pathname, searchParams])
 
